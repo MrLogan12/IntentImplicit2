ca39e6b42d600ac8f14c907866d51312da60f82f
Alvaro <roelxdjj@gmail.com>
2022-05-18 17:29:49 -0400

    ultimo

--- app/src/main/java/com/example/intentimplicit/MainActivity.java
+++ app/src/main/java/com/example/intentimplicit/MainActivity.java
@@ -1,20 +1,25 @@
 package com.example.intentimplicit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     //defnimos los atributos de las vistas
-   private EditText etTelefono;
-   private ImageButton btnLlamar, btnCamara;
-
+    private EditText etTelefono;
+    private ImageButton btnLlamar, btnCamara;
+    private final int PHONE_CODE = 100;
     private String numeroTelefono;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,23 +27,32 @@ public class MainActivity extends AppCompatActivity {
         inicializarVistas();
         obtenerInformacion();
         configurarIntentImplicito();
+        versionesNuevas();
+    }
+
+    private void versionesNuevas() {
+
     }
 
     private void configurarIntentImplicito() {
-        if(!numeroTelefono.isEmpty()) {
+        if (!numeroTelefono.isEmpty()) {
             //primer problema
             //las llamadas han cambiado desde la version 6 o sdk23
             //a partir de esa version se hace el codigo con algunos cambios
             //antes de esa version tenia otra manera de hacer el codigo
 
-             //validar si vesion de tu proyecto es mayor o igual
+            //validar si vesion de tu proyecto es mayor o igual
             //a la version de android donde cammbio su forma de procesar esta llamada
             //ej: SDK_INT = 24 ej: VERSION_CODES.M=230
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            //para versiones nuevas
-            }else{
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                //
+                //
+                requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, PHONE_CODE);
+                //para versiones nuevas
+
+            } else {
                 //versiones antiguas
-            configurarVersionesAntiguas();
+                configurarVersionesAntiguas();
             }
         }
 
@@ -50,26 +64,64 @@ public class MainActivity extends AppCompatActivity {
         //                 ->2 que datos quieres enviar en el intent
         //URI es como la URL de web donde configuras las cabceras
         // tu ruta donde quieres pasasr datos
-        Intent intentLlamada = new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+numeroTelefono));
-        startActivity(intentLlamada);
+        if (revisarPermisos(Manifest.permission.CALL_PHONE)) {
+            Intent intentLlamada = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + numeroTelefono));
+            startActivity(intentLlamada);
+        } else {
+            Toast.makeText(this, "permisos denegados", Toast.LENGTH_SHORT);
+        }
 
     }
 
-    private void obtenerInformacion(){
-        numeroTelefono= etTelefono.getText().toString();   }
+    private void obtenerInformacion() {
+        numeroTelefono = etTelefono.getText().toString();
+    }
 
     private void inicializarVistas() {
 
         etTelefono = findViewById(R.id.etTelefono);
         btnLlamar = findViewById(R.id.btnLlamar);
         btnCamara = findViewById(R.id.btnCamara);
+
     }
-    private boolean revisarPermisos(String permiso){
+
+    private boolean revisarPermisos(String permiso) {
         //android maneja los permisos de sta manera:
         //granted: permiso otrogado
         //denied: permiso no otrogado
         //validar si el permiso a evaluar en su aplicacion tiene  el valor de android
         // que maneja android para otorgar un permiso otrogado que seria
         //granted
+        int resultadoPermiso = checkCallingOrSelfPermission(permiso);
+        return resultadoPermiso == PackageManager.PERMISSION_GRANTED;
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //Verificar el servicio a evaluar
+        switch (requestCode) {
+            case PHONE_CODE:
+                String permiso = permissions[0];
+                int valorPermiso = grantResults[0];
+                //evitar que hayan errores
+                if (permiso.equals(Manifest.permission.CALL_PHONE)) {
+                    if (valorPermiso == PackageManager.PERMISSION_GRANTED) { //Evaluar si el permiso ha sido otorgado o denegado
+                        Intent intentLlamada = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + numeroTelefono));
+                        startActivity(intentLlamada);
+                    } else {
+                        Toast.makeText(this, "El permiso esta denegado", Toast.LENGTH_LONG);
+                    }
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                break;
+        }
     }
 }
+
+
+
+
